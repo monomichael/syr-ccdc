e40190b7257e69ba9512bfea6a2f0aab293614d1
Michael Zhang <lzhang43@syr.edu>
2014-10-29 12:51:13 -0400

    Working on header

--- header.php
+++ header.php
@@ -18,29 +18,31 @@
         </head>
         <body <?php body_class(); ?>>
 
-        <header id="primary-header">
-                <nav class="navbar navbar-default" role="navigation">
-                        <div class="navbar-inner">
-                                <div class="container">
-                                        <a class="navbar-brand" href="<?php echo site_url(); ?>" title="<?php esc_attr( bloginfo('name') ); ?>"><?php bloginfo('name'); ?></a>
-                                        <div class="navbar-header">
-                                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                                                        <span class="sr-only">Toggle navigation</span>
-                                                        <span class="icon-bar"></span>
-                                                        <span class="icon-bar"></span>
-                                                        <span class="icon-bar"></span>
-                                                </button>
-                                        </div><?php 
-                                        wp_nav_menu( array(
-                                                'container_class' => 'collapse navbar-collapse navbar-right navbar-ex1-collapse',
-                                                'menu_class'      => 'nav navbar-nav',
-                                                'menu_id'         => 'main-menu',
-                                                'walker'          => new syr_ccdc_Walker_Nav_Menu()
-                                        ) ); ?>
-                                </div>
-                        </div>
-                </nav>
-        </header>
+        <div class="header-container">
+            <header id="primary-header">
+                    <nav class="navbar navbar-default" role="navigation">
+                            <div class="navbar-inner">
+                                    <div class="container">
+                                            <a class="navbar-brand" href="<?php echo site_url(); ?>" title="<?php esc_attr( bloginfo('name') ); ?>"><?php bloginfo('name'); ?></a>
+                                            <div class="navbar-header">
+                                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                                                            <span class="sr-only">Toggle navigation</span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                    </button>
+                                            </div><?php 
+                                            wp_nav_menu( array(
+                                                    'container_class' => 'collapse navbar-collapse navbar-right navbar-ex1-collapse',
+                                                    'menu_class'      => 'nav navbar-nav',
+                                                    'menu_id'         => 'main-menu',
+                                                    'walker'          => new syr_ccdc_Walker_Nav_Menu()
+                                            ) ); ?>
+                                    </div>
+                            </div>
+                    </nav>
+            </header>
+        </div>
 
 <div id="main-content">
     <div class="container">
