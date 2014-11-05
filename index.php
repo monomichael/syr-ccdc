11a1613081b675af6bcbeb9aeecc42e38f527df4
Michael Zhang <lzhang43@syr.edu>
2014-11-05 15:40:39 -0500

    Remove duplicated #main-content

--- index.php
+++ index.php
@@ -9,22 +9,18 @@
 get_header();
 ?>
 
-<div id="main-content" class="main-content">
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<h1><?php the_title(); ?></h1>	
-				<?php the_content(); ?>
-			<?php endwhile; else: ?>
-				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-			<?php endif; ?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-			
-	<?php get_sidebar( 'content' ); ?>
-
-</div><!-- #main-content -->
+<div id="primary" class="content-area">
+	<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>	
+			<?php the_content(); ?>
+		<?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+		<?php endif; ?>
+	</div><!-- #content -->
+</div><!-- #primary -->
+		
+<?php get_sidebar( 'content' ); ?>
 
 <?php
 get_footer();
