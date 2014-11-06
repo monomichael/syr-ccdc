7f1036cb810c32609ab07948761d8d4e3ffcec8b
Michael Zhang <lzhang43@syr.edu>
2014-11-06 10:49:09 -0500

    Add post meta infos

--- index.php
+++ index.php
@@ -15,6 +15,24 @@ get_header();
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<article>
 					<h1><?php the_title(); ?></h1>
+					<span class="post-meta-infos">
+						<time class="date-container post-meta-info" itemprop="datePublished">
+							<?php the_date(); ?>
+						</time>
+						<span class="text-sep">/</span>
+						<span class="comment-container post-meta-info">
+							<a title="Comment on <?php the_title(); ?>" class="comments-link" href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
+						</span>
+						<span class="text-sep">/</span>
+						<span class="category-container post-meta-info">
+							in <?php the_category(' '); ?>
+						</span>
+						<span class="text-sep">/</span>
+						<span class="author-container post-meta-info">
+							by <?php the_author_link(); ?>
+						</span>
+					</span>
+
 					<div class="entry-content" itemprop="text">
 						<?php the_content(); ?>
 					</div>
