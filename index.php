542051c39731276bd65620933d8b50a7989e43ec
Michael Zhang <lzhang43@syr.edu>
2014-11-06 10:37:05 -0500

    Styling for article p and blockquote

--- index.php
+++ index.php
@@ -13,8 +13,12 @@ get_header();
 	<div id="primary" class="content-area col-md-8">
 		<div id="content" class="site-content" role="main">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<h1><?php the_title(); ?></h1>	
-				<?php the_content(); ?>
+				<article>
+					<h1><?php the_title(); ?></h1>
+					<div class="entry-content" itemprop="text">
+						<?php the_content(); ?>
+					</div>
+				</article>
 			<?php endwhile; else: ?>
 				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 			<?php endif; ?>
