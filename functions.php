1799e39e6c82b81bfee47795bc5fb7844d112bf9
Michael Zhang <lzhang43@syr.edu>
2014-11-10 12:30:53 -0500

    Customize search form

--- functions.php
+++ functions.php
@@ -83,3 +83,19 @@ function syr_ccdc_widgets_init() {
 }
 
 add_action( 'widgets_init', 'syr_ccdc_widgets_init' );
+
+/* Register customized search form
+ * @todo HTML5 
+ */
+function syr_ccdc_search_form( $form ) {
+    $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+    <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
+    </div>
+    </form>';
+
+    return $form;
+}
+
+add_filter( 'get_search_form', 'syr_ccdc_search_form' );
